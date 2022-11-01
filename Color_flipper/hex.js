b9de0feaa2fa44ec8810cd7deb1c4fb4be49e45b
unknown <shindesanchit33@gmail.com>
2022-11-01 10:42:05 +0530

    updated hex.js

--- Color_flipper/hex.js
+++ Color_flipper/hex.js
@@ -1,9 +1,20 @@
 const hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const btn = document.getElementById('btn').addEventListener("click",getchange);
+const color = document.querySelector(".color");
 
-const btn = document.getElementsByClassName('#btn btn-hero');
 
-btn.addEventListener("click")
+
+function getchange(){
+    document.body.style.backgroundColor = change();
+    color.textContent = change();
+}
+
 
 function change(){
-    onclick
+    let val ='#';
+      for(let i=0;i<6;i++){
+        random_number = Math.floor(Math.random()*hex.length)
+        val = val + hex[random_number];
+    }
+    return val;
 }
